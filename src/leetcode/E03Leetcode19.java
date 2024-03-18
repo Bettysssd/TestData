209e83f129d00996ad16aa288eeab84950bf9c1b
Lucas <1661323614@qq.com>
2024-03-18 07:19:13 +0000

    update src/leetcode/E03Leetcode19.java.
    Signed-off-by: Lucas <1661323614@qq.com>

--- src/leetcode/E03Leetcode19.java
+++ src/leetcode/E03Leetcode19.java
@@ -1,13 +1,25 @@
-package leetcode;
-
-import recursion.ListNode;
-
 public class E03Leetcode19 {
 
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
         ListNode s = new ListNode(-1, head);
         recursion(s, n);
+        return s.next;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode s = new ListNode(-1, head);
+        ListNode p1 = s;
+        ListNode p2 = s;
+        for (int i = 0; i < n + 1; i++) {
+            p2 = p2.next;
+        }
+
+        while (p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        p2.next = p2.next.next;
         return s.next;
     }
 
@@ -23,3 +35,4 @@ public class E03Leetcode19 {
         return nth + 1;
     }
 }
+
