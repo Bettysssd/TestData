2f851b7adc93f978840a43e4e77961998a826b42
Lucas <1661323614@qq.com>
2024-04-01 13:27:11 +0000

    update src/leetcode/E01Leetcode23.java.
    Signed-off-by: Lucas <1661323614@qq.com>

--- src/leetcode/E01Leetcode23.java
+++ src/leetcode/E01Leetcode23.java
@@ -1,9 +1,43 @@
 public class E01Leetcode23 {
-    public ListNode mergeKLists(ListNode[] lists) {
+    public static ListNode mergeKLists(ListNode[] lists) {
         MinHeap heap = new MinHeap(lists.length);
         for (ListNode h : lists) {
-            heap.offer(h);
+            //加入到小顶堆
+            if (h != null) {
+                heap.offer(h);
+            }
         }
-        return null;
+        // 准备一个链表 (A sentinel)
+        ListNode s = new ListNode(-1, null);
+        ListNode t = s;
+        while (!heap.isEmpty()) {
+            ListNode min = heap.poll();
+            t.next = min;
+            t = min;
+            if (min.next != null) {
+                heap.offer(min.next);
+            }
+
+        }
+        return s.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode[] lists = new ListNode[3];
+        ListNode l1 = new ListNode(1,
+                new ListNode(4,
+                        new ListNode(5, null)));
+        ListNode l2 = new ListNode(1,
+                new ListNode(3,
+                        new ListNode(4, null)));
+        ListNode l3 = new ListNode(2,
+                new ListNode(6,null));
+        lists[0] = l1;
+        lists[1] = l2;
+        lists[2] = l3;
+
+        System.out.println(mergeKLists(lists).toString());
+
+
     }
 }
